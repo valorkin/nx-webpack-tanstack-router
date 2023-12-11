164191204303af26fb1bc3f549051a1bc856d6ba
Dmitriy Shekhovtsov <valorkin@gmail.com>
2023-12-11 05:03:02 +0100

    chore: router issue

--- apps/stack/jest.config.ts
+++ apps/stack/jest.config.ts
@@ -1,11 +1,14 @@
 /* eslint-disable */
 export default {
-  displayName: 'nx-react-webpack',
+  displayName: 'stack',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      { jsc: { transform: { react: { runtime: 'automatic' } } } },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/nx-react-webpack',
+  coverageDirectory: '../../../coverage/apps/stack/stack',
 };
